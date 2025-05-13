29da93024f248baa3deb199492c1a3136ba1e922
ganshyamsharma <gsmsharma99@gmail.com>
2025-05-13 19:25:02 +0530

    updated the code

--- neutron_coincidence_counter.v
+++ neutron_coincidence_counter.v
@@ -1,3 +1,5 @@
+`timescale 1us/1ns
+
 module NWCC(
     input clk_1mhz, pulse_ip, reset_ip,
     output [12:0] ra_count_op, a_count_op, total_count_op
@@ -114,25 +116,3 @@ endmodule
 
 
 
-
-	/////////////////////////////////////	TEST BENCH	////////////////////////////////
-	module tb_cc();
-	
-    reg clk, inp, rst;
-    wire [23:0]racc, acc, tc;
-    coin_count cc1(clk, inp, rst, racc, acc, tc);
-    
-    always
-    #0.5 clk = ~clk;
-    
-    initial begin
-        clk = 1'b0;
-        rst = 1'b1;
-        inp = 1'b0;
-    #10 rst = 1'b0;
-    #100  inp = 1'b1;
-    #5 inp = 1'b0;
-    #1000 inp = 1'b1;
-    #5 inp = 1'b0;
-    end             
-endmodule
