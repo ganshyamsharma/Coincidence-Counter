565c0c142ce711bcdaf4876d0dc4b3ec89e6d0a9
ganshyamsharma <gsmsharma99@gmail.com>
2025-05-26 22:26:06 +0530

    corrected erroneous naming of some identifiers

--- neutron_coincidence_counter.v
+++ neutron_coincidence_counter.v
@@ -1,11 +1,11 @@
-`timescale 1us/1ns
-
-module NWCC(
+module nwcc #(parameter DATA_BITS = 24)
+    (
     input i_clk_1mhz, i_pulse_signal, i_reset,
-    output [12:0] o_r_plus_a_count, o_a_count, o_total_count
-);
-    wire [12:0] w_cnt1, w_cnt2, w_sub, w_add1, w_add2;
-    wire w_a, w_b, w_c;                                                   		//a-->8us delay    b-->128us delay   c-->1024us delay
+    output [DATA_BITS-1 : 0] o_r_plus_a_count, o_a_count, o_total_count
+    );
+    
+    wire [DATA_BITS-1 : 0] w_cnt1, w_cnt2, w_sub, w_add1, w_add2;
+    wire w_a, w_b, w_c;                                                   	//a-->8us delay    b-->128us delay   c-->1024us delay
     
     assign o_total_count = w_cnt1; 
      
@@ -13,66 +13,71 @@ module NWCC(
     delay128 d2(i_clk_1mhz, i_reset, w_a, w_b);
     delay1024 d3(i_clk_1mhz, i_reset, i_pulse_signal, w_c);
          
-    counter_13bit c1(w_a, i_reset, w_cnt1);
-    counter_13bit c2(w_b, i_reset, w_cnt2);
-    w_subtractor_13bit w_sub1(w_cnt1, w_cnt2, w_sub);
-    adder_13bit w_add1(w_sub, o_r_plus_a_count, w_add1);                     	// R+A adder
-    adder_13bit w_add2(w_sub, o_a_count, w_add2);                      			// A adder
-    register_13bit reg1(i_pulse_signal, i_reset, w_add1, o_r_plus_a_count);     // R+A register
-    register_13bit reg2(w_c, i_reset, w_add2, o_a_count);  						//A register   
+    counter c1(w_a, i_reset, w_cnt1);
+    counter c2(w_b, i_reset, w_cnt2);
+    subtractor sub1(w_cnt1, w_cnt2, w_sub);
+    adder add1(w_sub, o_r_plus_a_count, w_add1);                     		// R+A adder
+    adder add2(w_sub, o_a_count, w_add2);                      				// A adder
+    register reg1(i_pulse_signal, i_reset, w_add1, o_r_plus_a_count);     	// R+A register
+    register reg2(w_c, i_reset, w_add2, o_a_count);  						//A register   
 	
 endmodule
-
+//
 ////////////////////////////////////////////////////////////// Counter Unit
-
-module counter_13bit(
+//
+module counter #(parameter DATA_BITS = 24)
+(
     input clk, reset,
-    output reg [12:0] count
+    output reg [DATA_BITS-1:0] count
 );
     always @(posedge clk, posedge reset) begin
     if (reset)
-        count <= 13'd0;
+        count <= 0;
     else
         count <= count + 1'b1;
     end 
 endmodule
-
+//
 ///////////////////////////////////////////////////////////// Subtractor Unit
-
-module w_subtractor_13bit(
-    input [12:0] num1, num2,
-    output [12:0] result
+//
+module subtractor #(parameter DATA_BITS = 24)
+(
+    input [DATA_BITS-1:0] num1, num2,
+    output [DATA_BITS-1:0] result
 );
     assign result = num1 - num2;   
 endmodule
-
-///////////////////////////////////////////////////////////// Adder Unit
-
-module adder_13bit(
-    input [12:0] num1, num2,
-    output [12:0] result
+//
+//////////////////////////////////////////////////////////// Adder Unit
+//
+module adder #(parameter DATA_BITS = 24)
+(
+    input [DATA_BITS-1:0] num1, num2,
+    output [DATA_BITS-1:0] result
 );
     assign result = num1 + num2;   
 endmodule
-
+//
 //////////////////////////////////////////////////////////// Register Unit
-
-module register_13bit(
+//
+module register #(parameter DATA_BITS = 24)
+(
     input clk, reset,
-    input [12:0] inp,
-    output reg [12:0] out
+    input [DATA_BITS-1:0] inp,
+    output reg [DATA_BITS-1:0] out
 );
     always @(posedge clk or posedge reset) begin
         if(reset)
-            out <= 13'd0;
+            out <= 0;
         else
             out <= inp;
     end 
 endmodule
-
+//
 //////////////////////////////////////////////////////////// Delayers
-
+//
 //////////////////// 8us delay
+//
 module delay8 (
 input clk, reset, inp, 
 output out
@@ -85,8 +90,10 @@ output out
         else
             shifter <= {shifter[6:0], inp};
     end
-endmodule       
-/////////////////// 128us delay 
+endmodule  
+//     
+/////////////////// 128us delay
+// 
 module delay128 (
 input clk, reset, inp, 
 output out
@@ -99,8 +106,10 @@ output out
         else
             shifter <= {shifter[126:0], inp};
     end
-endmodule                   
+endmodule        
+//           
 ///////////////// 1024us delay 
+//
 module delay1024 (
 input clk, reset, inp, 
 output out
